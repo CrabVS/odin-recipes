bd6a8eecd7906769afff2792460615ee0f1791ea
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-09-22 13:37:21 +0200

    Add home and footer structure

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,33 @@
+import './home.css';
+import initiateRecipes from '../recipes/recipes';
+import initiateAddRecipe from '../add-recipe/add-recipe';
+
+const addListeners = function addListeners() {
+  const buttons = Array.from(document.querySelectorAll('.btn'));
+  buttons[0].addEventListener('click', () => {
+    initiateRecipes('Trending');
+  });
+
+  buttons[1].addEventListener('click', () => {
+    initiateAddRecipe();
+  });
+};
+
 const initiateHome = function initiateHome() {
   const main = document.querySelector('main');
-  main.innerHTML = `Home`;
+  main.innerHTML = `
+  <div class="home">
+    <h1>Welcome to OdinRecipes</h1>
+    <div class="content">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum facere hic incidunt ullam quidem accusamus eum sit quod, ipsam perspiciatis! Corporis blanditiis laborum quos eos eius sapiente nobis ut tempora id, expedita consequuntur ad laudantium possimus ipsa quod harum? Error, iusto adipisci eaque nobis placeat perspiciatis accusamus totam voluptate? Non.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dolores consequatur natus pariatur distinctio hic nesciunt nam quas qui officia? Adipisci nemo accusamus cupiditate accusantium animi ducimus perspiciatis quas quam, atque molestias rerum cum exercitationem voluptatem assumenda, voluptate minus earum nam molestiae itaque? Temporibus perspiciatis quos magni beatae recusandae esse quo voluptatem, minima tenetur exercitationem quia provident necessitatibus natus quasi nemo corrupti sed assumenda aliquid totam neque illum quisquam nisi. Blanditiis maxime similique deleniti ut?</p>
+        <div class="button-group">
+            <a class="btn">View Trending Recipes</a>
+            <a class="btn">Add your own Recipe</a>
+        </div>
+    </div>
+  </div>`;
+  addListeners();
 };
 
 export default initiateHome;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './reset.css';
 import './index.css';
 import initiateHeader from './components/header/header';
 import initiateHome from './components/home/home';
-import initiateAddRecipe from './components/add-recipe/add-recipe';
 
 initiateHeader();
-initiateAddRecipe();
+initiateHome();
